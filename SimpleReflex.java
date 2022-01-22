bea87cb9cf968e0fa413bc01963a683aa81c57c9
Nguyen Hoa Pham <nhpham27@gmail.com>
2022-01-22 13:00:28 -0600

    minor changes in rules table

--- SimpleReflex.java
+++ SimpleReflex.java
@@ -53,7 +53,7 @@ public class SimpleReflex {
 		this.actionTable.put("00100", new double[]{0.6, 0.2, 0.2, 0, 0, 0});
 		
 		this.actionTable.put("00101", new double[]{0.1, 0.4, 0.5, 0, 0, 0});
-		this.actionTable.put("00110", new double[]{0, 0.3, 0.3, 0, 0, 0.4});
+		this.actionTable.put("00110", new double[]{0, 0.5, 0.5, 0, 0, 0});
 		this.actionTable.put("00111", new double[]{0, 0.3, 0.3, 0.0, 0, 0.4});
 		this.actionTable.put("01000", new double[]{0.1, 0.2, 0.3, 0.0, 0.2, 0.2});
 		this.actionTable.put("01001", new double[]{0.1, 0.3, 0.3, 0, 0.2, 0.1});
